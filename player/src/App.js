e50db64ef5b24e751c7e350503b6202c505e9b25
calypsogaming098two@gmail.com <development@vtlcrafters.com>
2020-02-11 17:03:44 +0800

    updated code

--- player/src/App.js
+++ player/src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import Searchbar from "./components/Searchbar";
-import searchYoutube from "youtube-api-v3-search";
 
 class App extends React.Component {
   state = {
     isPlaying: false,
-    API_KEY: "AIzaSyDNGKLlOY5A1pjNSY2S3kSXRLHY8ao3UwU",
-    options: {
-      q: "nodejs",
-      part: "snippet",
-      type: "video",
-      maxResults: 8
-    },
     results: []
   };
 
@@ -22,50 +14,17 @@ class App extends React.Component {
       : this.setState({ isPlaying: false });
   }
 
-  callFunc = () => {
-    let holder = [];
-    searchYoutube(this.state.API_KEY, this.state.options)
-      .then(res => {
-        return res.items.map(x => {
-          holder.push({
-            videoId: x.id.videoId,
-            title: x.snippet.title
-          });
-        });
-      })
-      .then(() => {
-        this.setState({
-          results: holder
-        });
-      });
+  getSearchResults = childResults => {
+    this.setState({ results: childResults });
   };
 
-  componentDidMount() {
-    this.callFunc();
-  }
-
   render() {
-    if (this.state.results.length > 0) {
-      return (
-        <div>
-          {/* <ReactPlayer
-          url={}
-          playing={this.state.isPlaying}
-        /> */}
-          <Searchbar />
-          <ul>
-            {this.state.results.map(x => (
-              <li>
-                <p>{x.title}</p>
-                <p>{x.videoId}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-    } else {
-      return <h1>Getting Data...</h1>;
-    }
+    return (
+      <React.Fragment>
+        <Searchbar search={this.getSearchResults} />
+        {this.state.results}
+      </React.Fragment>
+    );
   }
 }
 
--- player/src/components/Searchbar.js
+++ player/src/components/Searchbar.js
@@ -1,10 +1,67 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import searchYoutube from "youtube-api-v3-search";
 
-function Searchbar() {
-  return (
-    <div>
-      <input placeholder="insert Something"></input>
-    </div>
-  );
+class Searchbar extends React.Component {
+  state = {
+    term: "",
+    API_KEY: "AIzaSyDNGKLlOY5A1pjNSY2S3kSXRLHY8ao3UwU",
+    options: {
+      q: "nodejs",
+      part: "snippet",
+      type: "video",
+      maxResults: 8
+    },
+    results: []
+  };
+
+  sendData(searchData) {
+    this.props.search(searchData);
+  }
+
+  setTerm(data) {
+    this.setState({
+      term: data
+    });
+  }
+
+  callFunc(term) {
+    let holder = [];
+    searchYoutube(this.state.API_KEY, {
+      q: term,
+      part: "snippet",
+      type: "video",
+      maxResults: 8
+    })
+      .then(res => {
+        return res.items.map(x => {
+          holder.push({
+            videoId: x.id.videoId,
+            title: x.snippet.title
+          });
+        });
+      })
+      .then(this.props.search(holder));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="insert Something"
+          //onChange={e => this.sendData(e.target.value)}
+          onChange={e => this.setTerm(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="search"
+          onSubmit={() => this.callFunc(this.state.term)}
+        />
+      </form>
+    );
+  }
 }
 export default Searchbar;
